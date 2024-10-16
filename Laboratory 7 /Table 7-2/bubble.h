912cc47deb56e0166161627a13bb43cfef9bbb1f
Anna Zolina <qamlzolina@tip.edu.ph>
2024-10-16 14:57:53 +0800

    Create bubble.h

---
+++ Laboratory 7 /Table 7-2/bubble.h
@@ -0,0 +1,18 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+#include <algorithm>  // For std::swap
+
+// Template function for bubble sort
+template <typename T>
+void bubbleSort(T arr[], size_t arrSize) {
+    for (size_t i = 0; i < arrSize - 1; i++) {
+        for (size_t j = 0; j < arrSize - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+#endif  // BUBBLESORT_H
