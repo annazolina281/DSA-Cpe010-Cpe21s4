61cbbfd7ebcb81507a26c979eae7feee97cec6a6
Anna Zolina <qamlzolina@tip.edu.ph>
2024-10-21 15:00:35 +0800

    Update main.py

--- Laboratory 8/Table 8-2. Shell Sort Technique/main.py
+++ Laboratory 8/Table 8-2. Shell Sort Technique/main.py
@@ -11,7 +11,7 @@ int main() {
 
     srand(static_cast<unsigned int>(time(0))); // Seed for random number generation
     for (int i = 0; i < size; ++i) {
-        arr[i] = rand() % 1000 + 1; // Fill the array with random values
+        arr[i] = rand() % 100 + 1; // Fill the array with random values between 1 and 100
     }
 
     cout << "Unsorted Array:\n";
