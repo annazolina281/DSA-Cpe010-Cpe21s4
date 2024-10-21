98389cbbd8b569e9f291d982d52c006ecbb971a4
Anna Zolina <qamlzolina@tip.edu.ph>
2024-10-21 14:28:55 +0800

    Create main.py

---
+++ Laboratory 8/Table 8-2. Shell Sort Technique/main.py
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "shellsort.h" // Include the shellsort header
+
+using namespace std;
+
+int main() {
+    const int size = 100;
+    int arr[size];
+
+    srand(static_cast<unsigned int>(time(0))); // Seed for random number generation
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 1000 + 1; // Fill the array with random values
+    }
+
+    cout << "Unsorted Array:\n";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    shellSort(arr, size); // Call the shell sort function
+
+    cout << "Sorted Array:\n";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
