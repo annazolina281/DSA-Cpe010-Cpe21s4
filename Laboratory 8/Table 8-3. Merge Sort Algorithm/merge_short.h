a0a3ebe937516da8cde7b0f30250605db85f1858
Anna Zolina <qamlzolina@tip.edu.ph>
2024-10-21 14:44:25 +0800

    Create merge_short.h

---
+++ Laboratory 8/Table 8-3. Merge Sort Algorithm/merge_short.h
@@ -0,0 +1,32 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+void merge(int arr[], int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    int L[n1], R[n2]; // Use stack allocation for simplicity
+
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[middle + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    }
+
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+}
+
+#endif // MERGE_SORT_H
