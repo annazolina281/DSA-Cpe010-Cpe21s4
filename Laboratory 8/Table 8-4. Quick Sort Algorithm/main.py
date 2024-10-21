7a73ba3d85cb8a07f0a557cd97059458e8f2351b
Anna Zolina <qamlzolina@tip.edu.ph>
2024-10-21 15:02:14 +0800

    Update main.py

--- Laboratory 8/Table 8-4. Quick Sort Algorithm/main.py
+++ Laboratory 8/Table 8-4. Quick Sort Algorithm/main.py
@@ -9,17 +9,23 @@ int main() {
     const int size = 100;
     int arr[size];
 
-    srand(time(0)); // Seed for random number generation
-    for (int& num : arr) num = rand() % 1000 + 1; // Fill array with random values
+    srand(static_cast<unsigned int>(time(0))); // Seed for random number generation
+    for (int& num : arr) {
+        num = rand() % 100 + 1; // Fill array with random values between 1 and 100
+    }
 
     cout << "Unsorted Array:\n";
-    for (int num : arr) cout << num << " ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
     cout << endl;
 
     quickSort(arr, 0, size - 1); // Call quick sort
 
     cout << "Sorted Array:\n";
-    for (int num : arr) cout << num << " ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
     cout << endl;
 
     return 0;
